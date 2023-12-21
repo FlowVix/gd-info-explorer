89a049bfcceda72cffa9bf31c2e643413debae41
FlowVix <flowvix@gmail.com>
2023-12-21 20:16:23 +0200

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+    enum Page {
+        OBJ_PROPS = "Object properties",
+        SOMETHING = "something in the future maybe",
+    }
+
+    let currentPage = Page.OBJ_PROPS;
+</script>
+
+<div
+    class="w-screen h-screen bg-gray-900 p-2 flex flex-col text-gray-100 font-saira"
+>
+    <div
+        class="h-16 w-full bg-gray-800 border-2 border-gray-500 rounded-lg flex items-center pl-2 shadow-lg"
+    >
+        <span class="font-bold text-3xl mr-4">GD Info Explorer</span>
+        <div>
+            {#each Object.values(Page) as page}
+                <button
+                    class={`${
+                        currentPage == page ? "bg-gray-600" : "bg-gray-700"
+                    } p-2 mx-2 rounded-md font-medium`}
+                    on:click={() => {
+                        currentPage = page;
+                    }}>{page}</button
+                >
+            {/each}
+        </div>
+    </div>
+    <div class="">
+        <!--  -->
+    </div>
+</div>
+
+<button>gaga</button>
---
+++ vite.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import FullReload from "vite-plugin-full-reload";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [svelte(), FullReload("src/**/*")],
+});
