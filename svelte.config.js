f47359d4509c16f7dadbf1e08dce9fea49fa8b2c
FlowVix <flowvix@gmail.com>
2025-10-21 05:57:34 +0300

    hjelp

--- svelte.config.js
+++ svelte.config.js
@@ -8,6 +8,9 @@ const config = {
     preprocess: vitePreprocess(),
 
     kit: {
+        // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
+        // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+        // See https://svelte.dev/docs/kit/adapters for more information about adapters.
         adapter: adapter({
             pages: "build",
             assets: "build",
@@ -15,7 +18,7 @@ const config = {
             precompress: false,
             strict: true,
             paths: {
-                base: "/gd-info-explorer",
+                base: process.argv.includes("dev") ? "" : process.env.BASE_PATH,
             },
         }),
     },
